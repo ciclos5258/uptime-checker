f37c17166ad9a9d18768f05a4a86137ea3b056a6
Maksim <ciclos52582@gmail.com>
2026-07-24 09:06:40 +0300

    DB logging added

--- backend/checker.py
+++ backend/checker.py
@@ -1,11 +1,29 @@
 import requests
+import sqlite3
+from datetime import datetime
 from array import array
-
+# cfg
 url_dict = {'homeclimatcontrol.ru': 'https://homeclimatcontrol.ru', 
             'red-hills-lab.ru': 'https://red-hills-lab.ru/', 
             'rendich76.ru': 'https://rendich76.ru'}
 
 api_dict = {'homeclimatcontrol.ru': 'https://homeclimatcontrol.ru/api/latest'}
+
+#DB init
+
+connection = sqlite3.connect('requests.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS requests(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp TEXT NOT NULL,
+    url TEXT NOT NULL,
+    status_code INTEGER,
+    api_answer TEXT
+    )
+''')
+
 
 def health_check():
 
@@ -18,6 +36,13 @@ def health_check():
     for name, url in url_dict.items():
         status = status_code_checker(url)
         print(f"{name}: {status}")
+        current_time = datetime.now().isoformat()
+        cursor.execute(
+            'INSERT INTO requests (timestamp, url, status_code) VALUES (?,?,?)',
+            (current_time, url, status)
+        )
+
+    connection.commit()
 
 # site apis checking
 def api_check():
@@ -28,7 +53,16 @@ def api_check():
     for name, url in api_dict.items():
         data_dict = homeclimatcontrol_api_check(url)
         success = data_dict['success']
-        print(f"homeclimatcontrol.ru/api/latest access: {success}")
+        current_time = datetime.now().isoformat()
+        cursor.execute(
+            'INSERT INTO requests (timestamp, url, api_answer) VALUES (?,?,?)',
+            (current_time, url, success)
+        )
+    connection.commit()
+    print(f"homeclimatcontrol.ru/api/latest access: {success}")
 
 health_check()
 api_check()
+
+cursor.close()
+connection.close()
