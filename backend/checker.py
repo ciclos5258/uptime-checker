e20fcff99085c8e53ddf7f248c6b7614ef11bff9
Maksim <ciclos52582@gmail.com>
2026-07-24 10:45:16 +0300

    Function optimization

--- backend/checker.py
+++ backend/checker.py
@@ -1,7 +1,7 @@
 import requests
 import sqlite3
 from datetime import datetime
-from array import array
+
 # cfg
 url_dict = {'homeclimatcontrol.ru': 'https://homeclimatcontrol.ru', 
             'red-hills-lab.ru': 'https://red-hills-lab.ru/', 
@@ -23,46 +23,41 @@ cursor.execute('''
     api_answer TEXT
     )
 ''')
-
-
-def health_check():
-
-    # site access checking
-
-    def status_code_checker(url):
-        r = requests.get(url, timeout=10)
-        return r.status_code
-
-    for name, url in url_dict.items():
-        status = status_code_checker(url)
-        print(f"{name}: {status}")
-        current_time = datetime.now().isoformat()
-        cursor.execute(
-            'INSERT INTO requests (timestamp, url, status_code) VALUES (?,?,?)',
-            (current_time, url, status)
+def db_saver(url, status_code=None, api_answer=None):
+    current_time = datetime.now().isoformat()
+    cursor.execute(
+        'INSERT INTO requests (timestamp, url, status_code, api_answer) VALUES (?,?,?,?)',
+        (current_time, url, status_code, api_answer)
         )
 
-    connection.commit()
+# site access checking4
+
+def status_code_checker(url):
+    r = requests.get(url, timeout=10)
+    return r.status_code
+
+def homeclimatcontrol_api_check(url):
+    r = requests.get(url, timeout=10)
+    return r.json()
+
+def health_check():
+    for name, url in url_dict.items():
+        status_code = status_code_checker(url)
+        print(f"{name}: {status_code}")
+        db_saver(url, status_code)
+        
 
 # site apis checking
 def api_check():
-    def homeclimatcontrol_api_check(url):
-        r = requests.get(url, timeout=10)
-        return r.json()
-
     for name, url in api_dict.items():
         data_dict = homeclimatcontrol_api_check(url)
-        success = data_dict['success']
-        current_time = datetime.now().isoformat()
-        cursor.execute(
-            'INSERT INTO requests (timestamp, url, api_answer) VALUES (?,?,?)',
-            (current_time, url, success)
-        )
-    connection.commit()
-    print(f"homeclimatcontrol.ru/api/latest access: {success}")
+        api_answer = data_dict['success']
+        db_saver(url, api_answer=api_answer)
+    print(f"homeclimatcontrol.ru/api/latest access: {api_answer}")
 
 health_check()
 api_check()
 
+connection.commit()
 cursor.close()
 connection.close()
